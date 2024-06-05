47017406f3ce9aad87da6c8f9dcad04af498a12f
Mathieu Lefebvre <49442766+Matlefebvre1234@users.noreply.github.com>
2024-06-05 12:58:30 -0400

    optimise data storage by limiting duplicated data

--- src/sink.rs
+++ src/sink.rs
@@ -2,6 +2,7 @@
 use substreams::{errors::Error, pb::substreams::Clock};
 use substreams_entity_change::tables::Tables;
 use substreams_entity_change::pb::entity::EntityChanges;
+use std::collections::HashMap;
 use crate::pb::erc20::supply::types::v1::TotalSupplies;
 use substreams_database_change::pb::database::{table_change::Operation, DatabaseChanges};
 
@@ -30,17 +31,18 @@ pub fn db_out(clock: Clock, supply: TotalSupplies) -> Result<DatabaseChanges, Er
     let block = clock.number.to_string();
     let timestamp = clock.timestamp.unwrap().seconds.to_string();
     let mut database_changes: DatabaseChanges = Default::default();
-  
+
     for event in supply.items {
         let address = &event.address;
-        let id  = format!("{}-{}", address.clone(), block.clone());
-
+        let id  = HashMap::from([("address".to_string(),address.clone()),("block_number".to_string(), block.clone())]);
+        
         database_changes
-        .push_change("TotalSupply",id, 0, Operation::Create)
+        .push_change_composite("TotalSupply",id, 0, Operation::Create)
         .change("address", (None, address))
         .change("supply", (None, event.supply))
         .change("block_number", (None,block.clone() ))
-        .change("timestamp", (None, timestamp.clone()));
+        .change("timestamp", (None, timestamp.clone()))
+        .change("version",  (None, 1));
     }
     Ok(database_changes)
 }
