a836b64761d0758d8ba1f9d8d0f4d249b2f16266
Mathieu Lefebvre <49442766+Matlefebvre1234@users.noreply.github.com>
2024-08-09 11:16:23 -0400

    delta instead of get store

--- src/sink.rs
+++ src/sink.rs
@@ -1,27 +1,26 @@
 
-use substreams::{errors::Error, pb::substreams::Clock, store::{StoreGet, StoreGetString}};
+use substreams::{errors::Error, pb::substreams::Clock};
 use std::collections::HashMap;
-use crate::pb::erc20::supply::types::v1::TotalSupplies;
 use substreams_database_change::pb::database::{table_change::Operation, DatabaseChanges};
-
+use substreams::store::{Deltas, DeltaString};
 
 
 #[substreams::handlers::map]
-pub fn db_out(clock: Clock, supply: TotalSupplies,s: StoreGetString) -> Result<DatabaseChanges, Error> {
+pub fn db_out(clock: Clock,s: Deltas<DeltaString>) -> Result<DatabaseChanges, Error> {
     let block = clock.number.to_string();
     let timestamp = clock.timestamp.unwrap().seconds.to_string();
     let mut database_changes: DatabaseChanges = Default::default();
 
-    for event in supply.items {
-        let address = &event.address;
-
-        if s.get_at(0, address).unwrap() != event.supply {
+    for event in s.deltas{
+        let address = &event.key;
+        
+        if event.new_value != event.old_value {
             let id  = HashMap::from([("address".to_string(),address.clone()),("block".to_string(), block.clone())]);
         
         database_changes
         .push_change_composite("supply",id, 0, Operation::Create)
         .change("contract", (None, address))
-        .change("supply", (None, event.supply))
+        .change("supply", (None, event.new_value))
         .change("block_num", (None,block.clone() ))
         .change("timestamp", (None, timestamp.clone()));
     }
